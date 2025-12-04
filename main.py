deaefc17cbbf08bf14a2031a2eb1f239f7009cf2
DanGuttt <123544055+DanGuttt@users.noreply.github.com>
2025-12-04 23:47:29 +0100

    Update main.py

--- main.py
+++ main.py
@@ -5,9 +5,10 @@ app = FastAPI()
 @app.get("/calcp")
 def calculerp(nombre: int):
     resultat = nombre+1
-    return {"resultat": resultat}
+    return {resultat}
 
 @app.get("/calcm")
 def calculerm(nombre: int):
     resultat = nombre-1
-    return {"resultat": resultat}
+    return {resultat}
+
